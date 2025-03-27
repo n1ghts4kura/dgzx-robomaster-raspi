4d1488e49c10640ddf4c0c4afc3030401d15a9d0
n1ghts4kura <1825259836@qq.com>
2025-03-27 17:45:30 +0800

    debug.

--- modules/robot.py
+++ modules/robot.py
@@ -8,5 +8,7 @@ class Robot:
         self.instance = robot.Robot()
         self.instance.initialize(conn_type="rndis")
 
+        print("机器人版本: " + self.instance.get_version)
+
     def __del__(self):
         self.instance.close()
