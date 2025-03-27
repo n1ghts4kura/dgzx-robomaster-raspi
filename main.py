fc58136ede60337d1587fa90be1b05183133757c
n1ghts4kura <1825259836@qq.com>
2025-03-27 17:43:55 +0800

    改用SkillManager类用于管理技能

--- main.py
+++ main.py
@@ -5,27 +5,6 @@ import RPi.GPIO as GPIO
 from modules.robot import Robot
 from modules.skills_manager import SkillManager
 
-SKILL_THREADS = List[Thread]
-
-# 将技能加载进一个线程文件夹中
-def load_skill_into_thread(robot: Robot, skill_manager: SkillManager) -> SKILL_THREADS:
-    skills_threads = []
-    skill_id = 1
-    for skill in skill_manager.skills:
-        # 创建线程
-        thread = Thread(target=skill, args=(robot, skill_id), name="Thread-" + skill_id)
-        skills_threads.append(thread)
-        skill_id += 1
-    return skills_threads
-
-# 加载指定技能
-def start_skill(skills_threads: SKILL_THREADS, skill_id: int) -> bool:
-    for skill in skills_threads:
-        if skill.name == "Thread-" + skill_id:
-            skill.start()
-            return True
-    return False
-
 # 使用到的8个GPIO口序号
 # 使用BOARD编号
 PIN_USED_LIST = (11, 12, 13, 15, 16, 18, 22, 29)
@@ -37,18 +16,15 @@ def main():
         GPIO.setup(pin, GPIO.IN)
 
     robot = Robot()
-    skill_manager = SkillManager()
-
-    if not skill_manager.load():
-        print("技能加载失败。")
-        return
-
-    skills_threads = load_skill_into_thread(robot, skill_manager)
+    skill_manager = SkillManager(robot)
+    if not skill_manager.load_skills():
+        print("技能加载失败")
+        return 
 
     while True:
         for pin_num in range(1, 9):
             if GPIO.input(pin_num) == GPIO.HIGH:
-                start_skill(skills_threads, pin_num)
+                skill_manager.start_skill(pin_num)
 
 if __name__ == "__main__":
     main()
--- modules/skills_manager.py
+++ modules/skills_manager.py
@@ -1,4 +1,6 @@
 from typing import List
+from threading import Thread
+from robomaster.robot import Robot
 import os
 import glob
 
@@ -8,11 +10,15 @@ SKILL_FUNCTION_TYPE = function[None, None]
 class SkillManager:
     skills: List[SKILL_FUNCTION_TYPE]
 
-    def __init__(self):
+    def __init__(self, robot: Robot):
+        self.robot = robot
         self.skills = []
 
+    def skill_id_builder(id: int):
+        return "skill-" + id
+
     # 加载技能
-    def load(self) -> bool:
+    def load_skills(self) -> bool:
         # 获取当前skills目录内所有技能文件
         current_directory = os.getcwd()
         skill_file_names = glob.glob(current_directory + "/skills/*")
@@ -21,11 +27,27 @@ class SkillManager:
             # 利用__import__函数动态导入技能并加载run函数至本类skills列表中
             skill = __import__("./skills/" + skill_file_name)
             try:
-                self.skills.append(skill.run)
+                skill_num = skill_file_name[0]
+                skill_id = SkillManager.skill_id_builder(skill_num)
+                self.skills[skill_id] = skill
             except Exception as e:
                 # 加载失败
-                print("请检查技能文件内run函数是否存在\n以下是报错信息：")
+                print("请检查技能文件内run函数是否存在\n以下是报错信息:")
                 print(e)
                 return False
         # 加载成功
         return True
+
+    # 启动指定技能
+    # @param skill_id 技能id 
+    # @return 返回启动是否成功
+    def start_skill(self, skill_id: int) -> bool:
+        skill = self.skills.get(SkillManager.skill_id_builder(skill_id))
+
+        if skill is None:
+            # 无此技能
+            return False
+        else:
+            thread = Thread(target = skill, args=(self.robot))   
+            thread.start()
+            return True
