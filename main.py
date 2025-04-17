0506de8209b81be6f48389ecd2e7d5f2ac30d0a2
n1ghts4kura <1825259836@qq.com>
2025-04-17 12:31:11 +0800

    大改前的删库。 改用明文sdk获取选手端状态。

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 # /usr/local/bin/python3.7.8
 
-import time
 from modules.utils.logger import logger as LOGGER
 from modules.robot.robot import Robot
 from modules.skill.skill_manager import SkillManager
-from modules.uart.uart_connection import UartConnection
 
 
 def main() -> bool:
@@ -19,21 +17,7 @@ def main() -> bool:
         LOGGER.error("技能加载失败")
         return False
     skill_manager.log_skills()
-    
-    # 技能指令处理函数
-    def handler(command, skill_number = -1):
-        if command == 0:
-            # 关闭技能
-            skill_manager.stop_skill()
-        elif command == 1:
-            skill_manager.start_skill(skill_manager)
-        else:
-            pass
 
-    conn = UartConnection(handler)
-
-    while True:
-        pass
 
 if __name__ == "__main__":
     LOGGER.info("程序开始运行")
